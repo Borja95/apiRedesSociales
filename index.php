6b0b16eb0e4b943cc73d6f9313e50b92cd1e4d9c
Borja95 <59398903+Borja95@users.noreply.github.com>
2020-06-28 21:34:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,9 @@ include "./encabezados.php";
 //Consultar
 if ($_SERVER['REQUEST_METHOD'] == 'GET')
 {
-    if (isset($_GET['id']))
-    {
-      $consulta= "SELECT * FROM redes_sociales where id=".$_GET['id'];
-      $resultado = consultaGet($consulta);  
-      echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
-	  }
-    else {
-      $consulta= "SELECT * FROM redes_sociales";
+      $consulta= "use heroku_c8381f705ba64e3; SELECT * FROM redes_sociales";
       $resultado = consultaGet($consulta);  
       echo json_encode($resultado->fetchAll());
-  }
   header("HTTP/1.1 200 OK");
   exit();
 }
