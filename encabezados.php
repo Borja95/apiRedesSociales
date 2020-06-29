7da634b0bea82ee6cee0070798fe42dd8864383b
Borja95 <59398903+Borja95@users.noreply.github.com>
2020-06-28 22:25:11 -0500

    Update encabezados.php

--- encabezados.php
+++ encabezados.php
@@ -5,8 +5,8 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 //header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
 //header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Credentials: true");
-header('content-type: application/json; charset=utf-8');
-header('Content-Type: application/json');
+//header("Access-Control-Allow-Credentials: true");
+//header('content-type: application/json; charset=utf-8');
+//header('Content-Type: application/json');
 
 ?>
